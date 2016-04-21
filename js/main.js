efd501b1f0c431dfca2933811d3be059382b5f14
seanloveall <sloveall@stevens.edu>
2016-04-20 23:58:39 -0400

    more ui stuff

--- js/main.js
+++ js/main.js
@@ -3,8 +3,43 @@ $(function() {
 		$("#menu").toggleClass("active");
 	});
 	
-	$("#history span").click(function() {
+	$("#history .title").click(function() {
 		$("#history").toggleClass("active");
 		$("#overlay").toggleClass("active");
 	});
+	
+	$("#overlay").click(function() {
+		$(this).removeClass("active");
+		$("#history").removeClass("active");
+		$("#popup").removeClass("active");
+	});
+	
+	$(".svg-alert").click(function() {
+		$("#popup").addClass("active");
+		$("#overlay").addClass("active");
+	});
+	
+	$("#popup .close").click(function() {
+		$("#popup").removeClass("active");
+		$("#overlay").removeClass("active");
+	});
+	
+	$(window).resize(function() {
+	  resize();
+	});
+	resize();
 });
+
+function resize()
+{
+	var winHeight = $(window).height();
+	// remove top bar height
+	winHeight -= $("#top").outerHeight();
+	//remove bottom bar height
+	winHeight -= ($("#bottom ul li").outerHeight()+20);
+	
+	// convert to points
+	winHeight *= 72 / 96;
+	
+	$("#map").attr("height", winHeight+"pt");
+}
