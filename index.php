7a04d16020e9927603b414b98debb8fc0d034cd8
seanloveall <sloveall@stevens.edu>
2016-04-25 19:49:18 -0400

    lol

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
     $questions = json_encode($data->getAccDecQuestions());
     $accept_stats = json_encode($data->getAcceptStats());
     $decline_stats = json_encode($data->getDeclineStats());
-	echo "<div style='display:none'>";
-    echo $questions;
-    echo "\n--------\n";
-    echo $accept_stats;
-    echo "\n--------\n";
-    echo $decline_stats;
-	echo "</div>";
+	//echo "<div style='display:none'>";
+    //echo $questions;
+    //echo "\n--------\n";
+    //echo $accept_stats;
+    //echo "\n--------\n";
+    //echo $decline_stats;
+	//echo "</div>";
 
 ?>
 
@@ -31,7 +31,18 @@
 		<script src="js/jquery-1.12.1.min.js" type="text/javascript"></script>
 		<script src="js/map.js" type="text/javascript"></script>
 		<script src="js/main.js" type="text/javascript"></script>
-	<body onload="startTime()">
+		
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-76827596-1', 'auto');
+			ga('send', 'pageview');
+		</script>
+		
+	<body>
 		<div id="overlay"></div>
         <div id="popup">
 			<div class="close"></div>
@@ -42,6 +53,10 @@
 				<li><label><input type="radio" name="sample" /> Option 2</label></li>
 				<li><label><input type="radio" name="sample" /> Option 3</label></li>
 			</ul>
+			<div class="accdec">
+				<div class="accept">Accept</div>
+				<div class="decline">Decline</div>
+			</div>
 		</div>
 		<div id="top">
 			<div id="history" data-step="1" data-intro="This will provide you with the history of your decisions">
@@ -109,7 +124,7 @@
 				<li class="sea">
 					<div class="graphic"><img src='./img/sea/5.png' /></div>
 					<div class="change">+3.9</div>
-					<div class="infa">
+					<div class="info">
 						<h4>Sea Level</h4>
 						<span>14 F since 1980</span>
 					</div>
@@ -119,14 +134,6 @@
         <script type="text/javascript" src="js/intro.js"></script>
 	</body>
 </html>
-
-
-
-
-
-
-
-
 
 
 
